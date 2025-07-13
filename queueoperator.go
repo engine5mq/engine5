d1cb534f291bdd6dc24461573d4284a75120001e
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-07-13 13:45:51 +0300

    some flags to wait complete and continue

--- queueoperator.go
+++ queueoperator.go
@@ -9,14 +9,36 @@ type QueueOperator struct {
 	instances []*ConnectedClient
 	waiting   []*Message
 	sent      []*Message
+	isWorking bool
+}
+
+func (op *QueueOperator) waitForFinish() {
+	for {
+		if !op.isWorking {
+			break
+		}
+	}
+}
+
+func (op *QueueOperator) hold() {
+	op.waitForFinish()
+
+	op.isWorking = true
+}
+
+func (op *QueueOperator) release() {
+	op.isWorking = false
 }
 
 func (op *QueueOperator) addConnectedClient(client *ConnectedClient) {
+	op.hold()
 	op.instances = append(op.instances, client)
 	client.SetOperator(op)
+	op.release()
 }
 
 func (op *QueueOperator) removeConnectedClient(clientId string) {
+	op.hold()
 	// currentIndex := slices.Index(op.instances, op)
 	var instances []*ConnectedClient = []*ConnectedClient{}
 	for i := 0; i < len(op.instances); i++ {
@@ -25,28 +47,37 @@ func (op *QueueOperator) removeConnectedClient(clientId string) {
 		}
 	}
 	op.instances = instances
+	op.release()
 }
 
 func (op *QueueOperator) addMessage(msg *Message) {
+	op.hold()
 	op.waiting = append(op.waiting, msg)
+	op.release()
 	go op.LoopMessages()
 }
 
 func (op *QueueOperator) LoopMessages() {
 	for {
+		op.hold()
 
 		waitingLength := len(op.waiting)
 		if waitingLength == 0 {
+			op.release()
 			break
 		}
+
 		oldWaitingListindp := make([]*Message, waitingLength)
 		copy(oldWaitingListindp, op.waiting)
+		op.release()
 
 		sent := []*Message{}
 		failed := []*Message{}
+
 		for messageIndex := 0; messageIndex < len(oldWaitingListindp); messageIndex++ {
 			msg := oldWaitingListindp[messageIndex]
 			if msg.status != MsgOpOk {
+				op.hold()
 				for instanceIndex := 0; instanceIndex < len(op.instances); instanceIndex++ {
 					instance := op.instances[instanceIndex]
 					hasSubject := slices.Contains(instance.listeningSubjects, msg.targetSubjectName)
@@ -62,12 +93,15 @@ func (op *QueueOperator) LoopMessages() {
 						instance.Write(pl)
 					}
 				}
+				op.release()
 				msg.status = MsgOpOk
 				msg.lastOperationTime = time.Now()
 				sent = append(sent, msg)
 			}
 		}
+		op.hold()
 		op.sent = sent
 		op.waiting = failed
+		op.release()
 	}
 }
