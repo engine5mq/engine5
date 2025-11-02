4bc8d89189bddd7d50f8189704b79548f4298346
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-11-02 19:38:18 +0300

    "seven" -Undyne🐟🏹

--- build-push-docker.sh
+++ build-push-docker.sh
@@ -1,5 +1,5 @@
 export IMAGE_NAME='hcangunduz/engine5'
-export IMAGE_TAG='0.0.6-alpha'
+export IMAGE_TAG='0.0.7-alpha'
 export DOCKER_FILE="./dockerfile"
 docker build --file ${DOCKER_FILE} -t ${IMAGE_NAME}:${IMAGE_TAG} .
 docker push ${IMAGE_NAME}:${IMAGE_TAG}
